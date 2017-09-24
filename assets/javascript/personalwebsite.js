aeb776c512ef5c2b00b44fcb1e8a27b3cce0b95d
Faraz Khalid <mynameisfarazkhalid@gmail.com>
2017-09-24 16:33:00 -0500

    Website
    Push

---
+++ assets/javascript/personalwebsite.js
@@ -0,0 +1,11 @@
+console.log("hello")
+
+var typed = new Typed('.element', {
+  strings: ["Software Engineer", "Web Developer", "Android Developer", "UI/UX Designer"],
+  typeSpeed: 50,
+  backSpeed: 30,
+  backDelay: 1000,
+  loop: true, loopCount: Infinity,
+  shuffle: true
+
+});
